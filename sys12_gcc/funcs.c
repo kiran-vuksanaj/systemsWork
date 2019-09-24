3b6ba9ca44328454693979c84abd8315c6058c69
Kiran Vuksanaj <mvuksanaj00@stuy.edu>
2019-09-24 21:57:05 +0100

    modifications to like, actually, work?

--- sys12_gcc/funcs.c
+++ sys12_gcc/funcs.c
@@ -1,3 +1,21 @@
-int arrayAvg(int *aryP,int aryLength);
-void copyArray(int *a0, int *a1, int size);
-int strLength(char *str);
+#include<stdlib.h>
+
+int arrayAvg(int *aryP,int aryLength){
+  int out = 0;
+  int i;
+  for( i = 0; i < aryLength; i++){
+    out += aryP[i];
+  }
+  return out / aryLength;
+}
+void copyArray(int *a0, int *a1, int size){
+  int i;
+  for( i = 0; i < size ; i++ ){
+    a1[i] = a0[i];
+  }
+}
+int strLength(char *str){
+  int size = 0;
+  while(str[size] != '\0') size++;
+  return size + 1;
+}
