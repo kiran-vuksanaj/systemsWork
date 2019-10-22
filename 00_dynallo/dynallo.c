503d6ceaf91c75812d7a3523c384dd126dff7592
Kiran Vuksanaj <kiran-vuksanaj@gmail.com>
2019-10-22 15:27:30 -0400

    some comments

--- 00_dynallo/dynallo.c
+++ 00_dynallo/dynallo.c
@@ -12,9 +12,15 @@ int main(){
   srand(seed);
   printf("seed (time): %lu\n",seed);
   size_t ARRAY_SIZE = rand() % 15;
+
+  // in class, it was stated that the array size had to be a literal, but this is succeeding while being a variable
+  // array size is random, its determined at runtime
+  // wouldn't this constitute dynamic allocation ?
   int ary[ ARRAY_SIZE ];
   printf("ARRAY_SIZE = %lu\n",ARRAY_SIZE);
   printf("sizeof( ary ) = %lu\n", sizeof( ary ) );
+
+  // below is not really necessary, just to illustrate the array
 
   int i;
   for(i = 0; i < ARRAY_SIZE; i++ ) {
